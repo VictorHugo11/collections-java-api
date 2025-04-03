9e83a73809a966804de8627521377a2ebf982716
Victor Hugoe <dev.victor011@gmail.com>
2025-04-03 20:47:08 -0300

    Update Set

--- src/main/java/set/OperacoesBasicas/Convidado.java
+++ src/main/java/set/OperacoesBasicas/Convidado.java
@@ -19,6 +19,18 @@ public class Convidado {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Convidado convidado = (Convidado) o;
+        return getCodigoConvite() == convidado.getCodigoConvite();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getCodigoConvite());
+    }
+
+    @Override
     public String toString() {
         return "Convidado{" +
                 "nome='" + nome + '\'' +

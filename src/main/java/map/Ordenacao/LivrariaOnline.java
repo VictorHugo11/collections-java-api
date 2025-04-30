028287d8dd9c310f2281993e591dfe52c4b97f4c
Victor Hugoe <dev.victor011@gmail.com>
2025-04-29 21:45:43 -0300

    Update class LivrariaOnline

--- src/main/java/map/Ordenacao/LivrariaOnline.java
+++ src/main/java/map/Ordenacao/LivrariaOnline.java
@@ -1,7 +1,6 @@
 package main.java.map.Ordenacao;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class LivrariaOnline {
     private Map<String, Livro> livros;
@@ -14,5 +13,50 @@ public class LivrariaOnline {
         livros.put(link, livro);
     }
 
+    public void removerTitulo(String titulo) {
+        List<String> chavesRemover = new ArrayList<>();
+        for (Map.Entry<String, Livro> entry : livros.entrySet()) {
+            if (entry.getValue().getTitulo().equalsIgnoreCase(titulo)) {
+                chavesRemover.add(entry.getKey());
+            }
+        }
+        for (String chave : chavesRemover) {
+            livros.remove(chave);
+        }
+    }
 
+    public Map<String, Livro> exibirLivrosOrdenadosPorPreco() {
+        List<Map.Entry<String, Livro>> livrosParaOrdenarPorPreco = new ArrayList<>(livros.entrySet());
+
+        Collections.sort(livrosParaOrdenarPorPreco, new Livro.ComparatorPorPreco());
+        Map<String, Livro> livrosOrdenadosPorPreco = new LinkedHashMap<>();
+
+        for (Map.Entry<String, Livro> entry : livrosParaOrdenarPorPreco) {
+            livrosOrdenadosPorPreco.put(entry.getKey(), entry.getValue());
+        }
+        return livrosOrdenadosPorPreco;
+    }
+
+    public Map<String, Livro> exibirLivrosOrdenadosPorAutor() {
+        List<Map.Entry<String, Livro>> livrosParaOrdenarPorAutor = new ArrayList<>(livros.entrySet());
+
+        Collections.sort(livrosParaOrdenarPorAutor, new Livro.ComparatorPorAutor());
+        Map<String, Livro> livrosOrdenadosPorAutor = new LinkedHashMap<>();
+
+        for (Map.Entry<String, Livro> entry : livrosParaOrdenarPorAutor) {
+            livrosOrdenadosPorAutor.put(entry.getKey(), entry.getValue());
+        }
+        return livrosOrdenadosPorAutor;
+    }
+
+    public Map<String, Livro> pesquisarLivrosPorAutor(String autor) {
+        Map<String, Livro> livrosPorAutor = new LinkedHashMap<>();
+        for (Map.Entry<String, Livro> entry: livros.entrySet()) {
+            Livro livro = entry.getValue();
+            if (livro.getAutor().equals(autor)) {
+                livrosPorAutor.put(entry.getKey(), livro);
+            }
+        }
+        return livrosPorAutor;
+    }
 }
